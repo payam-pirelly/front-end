daec0f0bf390f509ae513d38982d6b52414784c8
payam <p.siyami.aghdam@gmail.com>
2023-05-20 05:58:09 -0700

    add car out

---
+++ src/sections/car/car-out.js
@@ -0,0 +1,115 @@
+import React from "react";
+import Car360Viewer from "./car-360-viewer";
+import ResponsiveAppBar from "./app-bar";
+import ArrowIcon from "src/components/icon/arrow-icon";
+import { IconButton } from "@mui/material";
+import { Box } from "@mui/system";
+import InfoIcon from "src/components/icon/infoIcon";
+import MaximizeIcon from "src/components/icon/maximize-icon";
+import MinimizeIcon from "src/components/icon/minimize-icon";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useMemo } from "react";
+import Panorama from "./panorama";
+import CarVerticalList from "./car-vertical-list";
+import CarControls from "./car-controllers";
+
+export default function CarOut() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const renderContent = useMemo(() => {
+    switch (value) {
+      case 0:
+        return <Car360Viewer />;
+      case 1:
+        return <Panorama />;
+      case 2:
+        return <CarVerticalList />;
+      case 3:
+        return 4;
+      default:
+        break;
+    }
+  }, [value]);
+
+  return (
+    <>
+      <ResponsiveAppBar />
+      {renderContent}
+      <Box
+        sx={{
+          position: "absolute",
+          top: "4rem",
+          right: "1rem",
+        }}
+      >
+        <IconButton>
+          <ArrowIcon />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "4rem",
+          left: "2rem",
+        }}
+      >
+        <IconButton>
+          <InfoIcon />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "2rem",
+        }}
+      >
+        <IconButton>
+          <MaximizeIcon />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "55%",
+          left: "2rem",
+        }}
+      >
+        <IconButton>
+          <MinimizeIcon />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          width: "30rem",
+          bgcolor: "#B0CDFF",
+          position: "absolute",
+          bottom: 0,
+          borderRadius: 1,
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          centered
+          sx={{
+            ".css-1wf8b0h-MuiTabs-flexContainer": {
+              justifyContent: "space-around",
+            },
+          }}
+        >
+          <Tab label="Exterior" />
+          <Tab label="Interior" />
+          <Tab label="Gallery" />
+          <Tab label="Walk Around" />
+        </Tabs>
+      </Box>
+      <CarControls />
+    </>
+  );
+}
