d0db81018397a8d5ff623a605d98023c98b31dfa
payam <p.siyami.aghdam@gmail.com>
2023-05-20 06:18:58 -0700

    edit appbar

--- src/sections/car/app-bar.js
+++ src/sections/car/app-bar.js
@@ -3,36 +3,19 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import PropTypes from "prop-types";
-import CustomizedSwitches from "src/components/switch";
 import { toggledCarStatus } from "src/redux/car-slice";
 import CloseIcon from "@mui/icons-material/Close";
 import { useDispatch } from "react-redux";
-
-function Item(props) {
-  const { sx, ...other } = props;
-  return <Box {...other} />;
-}
-
-Item.propTypes = {
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
+import { FormControlLabel, Switch } from "@mui/material";
+import { exitFullscreen } from "src/utils/fullscreen";
+import HotspotIcon from "src/components/icon/hotspot-icon";
 
 function ResponsiveAppBar() {
   const dispatch = useDispatch();
 
-  const handleClick = (data) => {
-    if (data) dispatch(toggledCarStatus("car360"));
-    else dispatch(toggledCarStatus("panorama"));
-  };
-
   const handleClose = () => {
-    dispatch(toggledCarStatus("car360"));
-    // exitFullscreen();
+    dispatch(toggledCarStatus("main"));
+    exitFullscreen();
   };
 
   return (
@@ -40,7 +23,7 @@ function ResponsiveAppBar() {
       sx={{
         position: "absolute",
         top: 0,
-        background: "linear-gradient(180deg,rgba(0,0,0,.8),transparent)",
+        background: "linear-gradient(180deg, white ,transparent)",
       }}
     >
       <Box
@@ -52,19 +35,41 @@ function ResponsiveAppBar() {
           borderRadius: 1,
         }}
       >
-        <Item>
-          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-            View gallery
-          </Typography>
-        </Item>
-        <Item>
-          <CustomizedSwitches onClick={handleClick} />
-        </Item>
-        <Item>
-          <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+        <Box>
+          <FormControlLabel
+            sx={{
+              color: "blue",
+              background: "#B0CDFF",
+              borderRadius: 1,
+              padding: 1,
+            }}
+            value="start"
+            control={<Switch size="small" color="primary" />}
+            label={
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <HotspotIcon
+                  style={{
+                    marginRight: 3,
+                    fontSize: "small",
+                    color: "#0085FF",
+                  }}
+                />
+                <Typography color={"#0085FF"}>Hotspot</Typography>
+              </Box>
+            }
+            labelPlacement="start"
+          />
+        </Box>
+        <Box>
+          <IconButton edge="start" onClick={handleClose} aria-label="close">
             <CloseIcon />
           </IconButton>
-        </Item>
+        </Box>
       </Box>
     </AppBar>
   );
