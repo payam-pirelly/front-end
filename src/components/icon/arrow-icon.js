aebc1981ee8ba00ed0cd80952d812cb839e20325
payam <p.siyami.aghdam@gmail.com>
2023-05-20 06:01:12 -0700

    add arrow icon

---
+++ src/components/icon/arrow-icon.js
@@ -0,0 +1,20 @@
+import { SvgIcon } from "@mui/material";
+import React from "react";
+
+export default function ArrowIcon() {
+  return (
+    <SvgIcon
+      xmlns="http://www.w3.org/2000/svg"
+      width="62"
+      height="62"
+      viewBox="0 0 62 62"
+      fill="none"
+    >
+      <circle cx="31" cy="31" r="31" fill="#1862E3" fill-opacity="0.07" />
+      <path
+        d="M39.13 25.49L40.9 27.27L31 37.16L21.1 27.26L22.87 25.49L31 33.62L39.13 25.49V25.49Z"
+        fill="#0085FF"
+      />
+    </SvgIcon>
+  );
+}
